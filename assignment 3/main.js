6ab372107a11714625ec0a5b31dd19aa98008124
Ruba Saeed] <161022394+RubaSaeed12@users.noreply.github.com>
2024-06-11 09:02:14 -0700

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,4 @@
+//storing the famous quote and its author
+var quote = "You never fail until you stop trying";
+var author = "Albert Einstein";
+console.log("\"".concat(quote, "\" ").concat(author));
---
+++ assignment 3/main.ts
@@ -0,0 +1,4 @@
+//storing the famous quote and its author
+let quote = "You never fail until you stop trying"
+let author = "Albert Einstein"
+console.log(`"${quote}" ${author}`)
