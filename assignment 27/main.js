1756752cb8110daa8065484e792429d1be39d2fd
Ruba Saeed] <161022394+RubaSaeed12@users.noreply.github.com>
2024-06-22 09:10:31 -0700

    first commit

---
+++ assignment 27/main.js
@@ -0,0 +1,10 @@
+var alienColor = "Green";
+if (alienColor === "Green") {
+    console.log("you shot down green alien you have earned 5 points");
+}
+else if (alienColor === "Yellow") {
+    console.log("you shot down yellow alien you have earned 10 points");
+}
+else if (alienColor === "Red") {
+    console.log("you shot down red alien you have earned 15 points");
+}
---
+++ assignment 27/main.ts
@@ -0,0 +1,37 @@
+// \\\\version 1
+let alienColor = "Green"
+if (alienColor === "Green") {
+console.log("you shot down green alien you have earned 5 points")    
+}
+else if (alienColor === "Yellow") {
+console.log("you shot down yellow alien you have earned 10 points")     
+    
+} 
+else if (alienColor === "Red") {
+ console.log("you shot down red alien you have earned 15 points")  
+}
+//VERSION 2
+let alienColor2 = "Yellow"
+if (alienColor2 === "Green") {
+console.log("you shot down green alien you have earned 5 points")    
+}
+else if (alienColor2 === "Yellow") {
+console.log("you shot down yellow alien you have earned 10 points")     
+    
+} 
+else if (alienColor2 === "Red") {
+ console.log("you shot down red alien you have earned 15 points")  
+}
+
+//VERSION 3
+let alienColor3 = "Red"
+if (alienColor3 === "Green") {
+console.log("you shot down green alien you have earned 5 points")    
+}
+else if (alienColor3 === "Yellow") {
+console.log("you shot down yellow alien you have earned 10 points")     
+    
+} 
+else if (alienColor3 === "Red") {
+ console.log("you shot down red alien you have earned 15 points")  
+}
